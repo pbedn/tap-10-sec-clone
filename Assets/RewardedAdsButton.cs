a0fd7acb10625105997acbf18aba598040ae1d79
Paweł Bednarz <pbedn@users.noreply.github.com>
2020-10-07 21:56:11 +0200

    Add initial game

---
+++ Assets/RewardedAdsButton.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+[RequireComponent(typeof(Button))]
+public class RewardedAdsButton : MonoBehaviour, IUnityAdsListener
+{
+
+#if UNITY_IOS
+    private string gameId = "3808988";
+#elif UNITY_ANDROID
+    private string gameId = "3808989";
+#endif
+
+    Button myButton;
+    public string myPlacementId = "rewardedVideo";
+
+    void Start()
+    {
+        myButton = GetComponent<Button>();
+
+        // Set interactivity to be dependent on the Placement’s status:
+        myButton.interactable = Advertisement.IsReady(myPlacementId);
+
+        // Map the ShowRewardedVideo function to the button’s click listener:
+        if (myButton)
+        {
+            myButton.onClick.AddListener(ShowRewardedVideo);
+        }
+        else
+        {
+            Debug.Log("Rewarded video is not ready at the moment! Please try again later!");
+        }
+
+        // Initialize the Ads listener and service:
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, true);
+    }
+
+    // Implement a function for showing a rewarded video ad:
+    void ShowRewardedVideo()
+    {
+        Advertisement.Show(myPlacementId);
+    }
+
+    // Implement IUnityAdsListener interface methods:
+    public void OnUnityAdsReady(string placementId)
+    {
+        // If the ready Placement is rewarded, activate the button: 
+        if (placementId == myPlacementId)
+        {
+            myButton.interactable = true;
+        }
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        // Define conditional logic for each ad completion status:
+        if (showResult == ShowResult.Finished)
+        {
+            // Reward the user for watching the ad to completion.
+            print("Ad was presented. Added 100 coins");
+            UIManager.Instance.UpdateCoinTotal(100);
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            // Do not reward the user for skipping the ad.
+            print("You skipped the ad, no coin for you");
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            Debug.LogWarning("The ad did not finish due to an error.");
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        // Log the error.
+        print("AD DID ERROR");
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        // Optional actions to take when the end-users triggers an ad.
+        print("AD DID START");
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("UIManager is NULL");
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void UpdateCoinTotal(int coins)
+    {
+        print("Giving the player " + coins + " coins");
+        GameManager.Instance.totalCoins += coins;
+    }
+}
